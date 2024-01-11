61020d561d5a59e548f209c0f1a696c9813d32b3
Dereje Getahun <getahun.d@gmail.com>
2024-01-11 13:38:42 +0300

    connect client to API

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  /* Because of StrictMode CDM is running twice and so making API calls twice in generate cover letter - removed, */
+  <App />
 )
